83c34f01d68ff93730e391d803c15072a74c2ce9
Rose Silletto <ras5491@rit.edu>
2025-04-24 18:16:17 -0400

    fixed errors

--- server/app.js
+++ server/app.js
@@ -2,23 +2,23 @@ const path = require('path');
 const express = require('express');
 const compression = require('compression');
 const favicon = require('serve-favicon');
-const bodyParse = require('body-parser');
+// const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const expressHandlebars = require('express-handlebars');
 const helmet = require('helmet');
 
-const router = require('./router.js');
 const bodyParser = require('body-parser');
+const router = require('./router.js');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
 const dbURI = process.env.MONGODB_URI || 'mongodb://127.0.0.1/DomoMaker';
 
 mongoose.connect(dbURI).catch((err) => {
-    if(err) {
-        console.log('Could not connect to database');
-        throw err;
-    }
+  if (err) {
+    console.log('Could not connect to database');
+    throw err;
+  }
 });
 
 const app = express();
@@ -27,7 +27,7 @@ app.use(helmet());
 app.use('/assets', express.static(path.resolve(`${__dirname}/../hosted`)));
 app.use(favicon(`${__dirname}/../hosted/img/favicon.png`));
 app.use(compression());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.engine('handlebars', expressHandlebars.engine({ defaultLayout: '' }));
 app.set('view engine', 'handlebars');
@@ -36,6 +36,6 @@ app.set('views', `${__dirname}/../views`);
 router(app);
 
 app.listen(port, (err) => {
-    if (err) {throw err; }
-    console.log(`Listening on port ${port}`);
+  if (err) { throw err; }
+  console.log(`Listening on port ${port}`);
 });
